7df0bb2efe1b960d93f9756b315e5494f33adafc
DESKTOP-JQBL71S\dungn <dungndth@gmail.com>
2018-03-30 17:41:10 +0700

    firt comit

---
+++ HocMVCAngular2/HocMVCAngular2/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace HocMVCAngular2.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
